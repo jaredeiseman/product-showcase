377d33cd72934070d6dce35dd351816bccd61053
Jared Eiseman <jathei@gmail.com>
2017-05-26 14:35:39 -0700

    add photo box and backdrop, add expected delivery date

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var date = new Date();
+  var numberOfDaysToAdd = 6;
+  date.setDate(date.getDate() + 2);
+
+  $('#date').text(date.toDateString());
+
   $('.alt-img').mouseover(function() {
     $('.featured-img').prop('src', $(this).prop('src'));
   }).mouseleave(function() {
@@ -12,5 +18,14 @@ $(document).ready(function() {
 
   $('form').submit(function(e) {
     e.preventDefault();
+  });
+
+  $('img').click(function() {
+    $('.photo-box').prop('src', $(this).prop('src'));
+    $('.photo-box, .backdrop').show();
+  });
+
+  $('.photo-box, .backdrop').click(function() {
+    $('.photo-box, .backdrop').hide();
   });
 });
